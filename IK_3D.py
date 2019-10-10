4a7658a890c58df6518253969664b9f599641cf6
Ryan Fainor <fainorr@lafayette.edu>
2019-10-10 10:25:51 -0400

    Update IK_3D.py

--- IK_3D.py
+++ IK_3D.py
@@ -49,34 +49,34 @@ leg4_offset = 0 		# back right
 t = linspace(0,gait_duration,1000)
 
 # zeros x, y, z, angs, angf, angt
-if (1==1):
-	x1 = zeros(len(t))
-	y1 = zeros(len(t))
-	z1 = zeros(len(t))
-	angs1 = zeros(len(t))
-	angf1 = zeros(len(t))
-	angt1 = zeros(len(t))
 
-	x2 = zeros(len(t))
-	y2 = zeros(len(t))
-	z2 = zeros(len(t))
-	angs2 = zeros(len(t))
-	angf2 = zeros(len(t))
-	angt2 = zeros(len(t))
+x1 = zeros(len(t))
+y1 = zeros(len(t))
+z1 = zeros(len(t))
+angs1 = zeros(len(t))
+angf1 = zeros(len(t))
+angt1 = zeros(len(t))
 
-	x3 = zeros(len(t))
-	y3 = zeros(len(t))
-	z3 = zeros(len(t))
-	angs3 = zeros(len(t))
-	angf3 = zeros(len(t))
-	angt3 = zeros(len(t))
+x2 = zeros(len(t))
+y2 = zeros(len(t))
+z2 = zeros(len(t))
+angs2 = zeros(len(t))
+angf2 = zeros(len(t))
+angt2 = zeros(len(t))
 
-	x4 = zeros(len(t))
-	y4 = zeros(len(t))
-	z4 = zeros(len(t))
-	angs4 = zeros(len(t))
-	angf4 = zeros(len(t))
-	angt4 = zeros(len(t))
+x3 = zeros(len(t))
+y3 = zeros(len(t))
+z3 = zeros(len(t))
+angs3 = zeros(len(t))
+angf3 = zeros(len(t))
+angt3 = zeros(len(t))
+
+x4 = zeros(len(t))
+y4 = zeros(len(t))
+z4 = zeros(len(t))
+angs4 = zeros(len(t))
+angf4 = zeros(len(t))
+angt4 = zeros(len(t))
 
 
 # develop functions for foot positions for given gait
@@ -105,15 +105,21 @@ for i in range(0,len(t)):
 
 #  to solve for servo angles As (shoulder), Af (femur), and At (tibia)
 
-def getServoAng(x, y, z, ls, lf, lt):
+def getServoAng(x, y, z, ls, lf, lt, leg):
 
 	if (y<0):
 		Adxy = arctan(z/y)
 	else:
 		Adxy = pi + arctan(z/y)
 
+	if (leg == 1 or leg == 3):
+		Adxy = pi-Adxy
+
 	dxy = sqrt(y**2 + z**2)
 	As = Adxy - arccos(ls/dxy)
+
+	if (leg == 1 or leg == 3):
+		As = pi-As
 
 	if (x<0):
 		Ad = arctan((z+ls*sin(As))/x)
@@ -151,74 +157,74 @@ figyz.suptitle('Front View y,z', fontsize=16)
 
 
 # initialize elements in plots
-if (1==1):
-	slinexz1, = ax1.plot([],[],lw=5,c='0.7')
-	flinexz1, = ax1.plot([],[],lw=5,c='0.7')
-	tlinexz1, = ax1.plot([],[],lw=5,c='0.7')
-	targetxz1, = ax1.plot([],[],lw=5,c='b')
 
-	slinexy1, = ax2.plot([],[],lw=5,c='0.7')
-	flinexy1, = ax2.plot([],[],lw=5,c='0.7')
-	tlinexy1, = ax2.plot([],[],lw=5,c='0.7')
-	targetxy1, = ax2.plot([],[],lw=5,c='b')
+slinexz3, = ax1.plot([],[],lw=5,c='0.7')
+flinexz3, = ax1.plot([],[],lw=5,c='0.7')
+tlinexz3, = ax1.plot([],[],lw=5,c='0.7')
+targetxz3, = ax1.plot([],[],lw=5,c='0.7')
 
-	slineyz1, = ax3.plot([],[],lw=5,c='0.7')
-	flineyz1, = ax3.plot([],[],lw=5,c='0.7')
-	tlineyz1, = ax3.plot([],[],lw=5,c='0.7')
-	targetyz1, = ax3.plot([],[],lw=5,c='b')
+targetxy3, = ax2.plot([],[],lw=5,c='0.7')
+tlinexy3, = ax2.plot([],[],lw=5,c='0.7')
+flinexy3, = ax2.plot([],[],lw=5,c='0.4')
+slinexy3, = ax2.plot([],[],lw=5,c='0.4')
 
-
-	slinexz3, = ax1.plot([],[],lw=5,c='0.7')
-	flinexz3, = ax1.plot([],[],lw=5,c='0.7')
-	tlinexz3, = ax1.plot([],[],lw=5,c='0.7')
-	targetxz3, = ax1.plot([],[],lw=5,c='0.7')
-
-	slinexy3, = ax2.plot([],[],lw=5,c='0.7')
-	flinexy3, = ax2.plot([],[],lw=5,c='0.7')
-	tlinexy3, = ax2.plot([],[],lw=5,c='0.7')
-	targetxy3, = ax2.plot([],[],lw=5,c='0.7')
-
-	slineyz3, = ax3.plot([],[],lw=5,c='0.7')
-	flineyz3, = ax3.plot([],[],lw=5,c='0.7')
-	tlineyz3, = ax3.plot([],[],lw=5,c='0.7')
-	targetyz3, = ax3.plot([],[],lw=5,c='0.7')
+slineyz3, = ax3.plot([],[],lw=5,c='0.7')
+flineyz3, = ax3.plot([],[],lw=5,c='0.7')
+tlineyz3, = ax3.plot([],[],lw=5,c='0.7')
+targetyz3, = ax3.plot([],[],lw=5,c='0.7')
 
 
-	spinexz, = ax1.plot([],[],lw=8,c='0.5')
-	spinexy, = ax2.plot([],[],lw=8,c='0.5')
-	spineyz, = ax3.plot([],[],lw=8,c='0.5')
+slinexz1, = ax1.plot([],[],lw=5,c='0.7')
+flinexz1, = ax1.plot([],[],lw=5,c='0.7')
+tlinexz1, = ax1.plot([],[],lw=5,c='0.7')
+targetxz1, = ax1.plot([],[],lw=5,c='b')
+
+tlinexy1, = ax2.plot([],[],lw=5,c='0.7')
+targetxy1, = ax2.plot([],[],lw=5,c='b')
+flinexy1, = ax2.plot([],[],lw=5,c='0.4')
+slinexy1, = ax2.plot([],[],lw=5,c='0.4')
+
+slineyz1, = ax3.plot([],[],lw=5,c='0.4')
+flineyz1, = ax3.plot([],[],lw=5,c='0.4')
+tlineyz1, = ax3.plot([],[],lw=5,c='0.4')
+targetyz1, = ax3.plot([],[],lw=5,c='b')
 
 
-	slinexz2, = ax1.plot([],[],lw=5,c='0.4')
-	flinexz2, = ax1.plot([],[],lw=5,c='0.4')
-	tlinexz2, = ax1.plot([],[],lw=5,c='0.4')
-	targetxz2, = ax1.plot([],[],lw=5,c='0.4')
-
-	slinexy2, = ax2.plot([],[],lw=5,c='0.4')
-	flinexy2, = ax2.plot([],[],lw=5,c='0.4')
-	tlinexy2, = ax2.plot([],[],lw=5,c='0.4')
-	targetxy2, = ax2.plot([],[],lw=5,c='0.4')
-
-	slineyz2, = ax3.plot([],[],lw=5,c='0.4')
-	flineyz2, = ax3.plot([],[],lw=5,c='0.4')
-	tlineyz2, = ax3.plot([],[],lw=5,c='0.4')
-	targetyz2, = ax3.plot([],[],lw=5,c='0.4')
+spinexz, = ax1.plot([],[],lw=8,c='0.5')
+spinexy, = ax2.plot([],[],lw=8,c='0.5')
+spineyz, = ax3.plot([],[],lw=8,c='0.5')
 
 
-	slinexz4, = ax1.plot([],[],lw=5,c='0.4')
-	flinexz4, = ax1.plot([],[],lw=5,c='0.4')
-	tlinexz4, = ax1.plot([],[],lw=5,c='0.4')
-	targetxz4, = ax1.plot([],[],lw=5,c='0.4')
+slinexz4, = ax1.plot([],[],lw=5,c='0.4')
+flinexz4, = ax1.plot([],[],lw=5,c='0.4')
+tlinexz4, = ax1.plot([],[],lw=5,c='0.4')
+targetxz4, = ax1.plot([],[],lw=5,c='0.4')
 
-	slinexy4, = ax2.plot([],[],lw=5,c='0.4')
-	flinexy4, = ax2.plot([],[],lw=5,c='0.4')
-	tlinexy4, = ax2.plot([],[],lw=5,c='0.4')
-	targetxy4, = ax2.plot([],[],lw=5,c='0.4')
+targetxy4, = ax2.plot([],[],lw=5,c='0.7')
+tlinexy4, = ax2.plot([],[],lw=5,c='0.7')
+flinexy4, = ax2.plot([],[],lw=5,c='0.4')
+slinexy4, = ax2.plot([],[],lw=5,c='0.4')
 
-	slineyz4, = ax3.plot([],[],lw=5,c='0.4')
-	flineyz4, = ax3.plot([],[],lw=5,c='0.4')
-	tlineyz4, = ax3.plot([],[],lw=5,c='0.4')
-	targetyz4, = ax3.plot([],[],lw=5,c='0.4')
+slineyz4, = ax3.plot([],[],lw=5,c='0.7')
+flineyz4, = ax3.plot([],[],lw=5,c='0.7')
+tlineyz4, = ax3.plot([],[],lw=5,c='0.7')
+targetyz4, = ax3.plot([],[],lw=5,c='0.7')
+
+
+slinexz2, = ax1.plot([],[],lw=5,c='0.4')
+flinexz2, = ax1.plot([],[],lw=5,c='0.4')
+tlinexz2, = ax1.plot([],[],lw=5,c='0.4')
+targetxz2, = ax1.plot([],[],lw=5,c='0.4')
+
+targetxy2, = ax2.plot([],[],lw=5,c='0.7')
+tlinexy2, = ax2.plot([],[],lw=5,c='0.7')
+flinexy2, = ax2.plot([],[],lw=5,c='0.4')
+slinexy2, = ax2.plot([],[],lw=5,c='0.4')
+
+slineyz2, = ax3.plot([],[],lw=5,c='0.4')
+flineyz2, = ax3.plot([],[],lw=5,c='0.4')
+tlineyz2, = ax3.plot([],[],lw=5,c='0.4')
+targetyz2, = ax3.plot([],[],lw=5,c='0.4')
 
 x_ann_list = []
 y_ann_list = []
@@ -234,72 +240,69 @@ def init():
 
 	# spine.set_data([],[])
 
-	if (1==1):
+	spinexz.set_data([],[])
+	spinexy.set_data([],[])
+	spineyz.set_data([],[])
 
-		spinexz.set_data([],[])
-		spinexy.set_data([],[])
-		spineyz.set_data([],[])
+	slinexz1.set_data([],[])
+	flinexz1.set_data([],[])
+	tlinexz1.set_data([],[])
+	targetxz1.set_data([],[])
 
+	slinexy1.set_data([],[])
+	flinexy1.set_data([],[])
+	tlinexy1.set_data([],[])
+	targetxy1.set_data([],[])
 
-		slinexz1.set_data([],[])
-		flinexz1.set_data([],[])
-		tlinexz1.set_data([],[])
-		targetxz1.set_data([],[])
+	slineyz1.set_data([],[])
+	flineyz1.set_data([],[])
+	tlineyz1.set_data([],[])
+	targetyz1.set_data([],[])
 
-		slinexy1.set_data([],[])
-		flinexy1.set_data([],[])
-		tlinexy1.set_data([],[])
-		targetxy1.set_data([],[])
+	slinexz2.set_data([],[])
+	flinexz2.set_data([],[])
+	tlinexz2.set_data([],[])
+	targetxz2.set_data([],[])
 
-		slineyz1.set_data([],[])
-		flineyz1.set_data([],[])
-		tlineyz1.set_data([],[])
-		targetyz1.set_data([],[])
+	slinexy2.set_data([],[])
+	flinexy2.set_data([],[])
+	tlinexy2.set_data([],[])
+	targetxy2.set_data([],[])
 
-		slinexz2.set_data([],[])
-		flinexz2.set_data([],[])
-		tlinexz2.set_data([],[])
-		targetxz2.set_data([],[])
+	slineyz2.set_data([],[])
+	flineyz2.set_data([],[])
+	tlineyz2.set_data([],[])
+	targetyz2.set_data([],[])
 
-		slinexy2.set_data([],[])
-		flinexy2.set_data([],[])
-		tlinexy2.set_data([],[])
-		targetxy2.set_data([],[])
+	slinexz3.set_data([],[])
+	flinexz3.set_data([],[])
+	tlinexz3.set_data([],[])
+	targetxz3.set_data([],[])
 
-		slineyz2.set_data([],[])
-		flineyz2.set_data([],[])
-		tlineyz2.set_data([],[])
-		targetyz2.set_data([],[])
+	slinexy3.set_data([],[])
+	flinexy3.set_data([],[])
+	tlinexy3.set_data([],[])
+	targetxy3.set_data([],[])
 
-		slinexz3.set_data([],[])
-		flinexz3.set_data([],[])
-		tlinexz3.set_data([],[])
-		targetxz3.set_data([],[])
+	slineyz3.set_data([],[])
+	flineyz3.set_data([],[])
+	tlineyz3.set_data([],[])
+	targetyz3.set_data([],[])
 
-		slinexy3.set_data([],[])
-		flinexy3.set_data([],[])
-		tlinexy3.set_data([],[])
-		targetxy3.set_data([],[])
+	slinexz4.set_data([],[])
+	flinexz4.set_data([],[])
+	tlinexz4.set_data([],[])
+	targetxz4.set_data([],[])
 
-		slineyz3.set_data([],[])
-		flineyz3.set_data([],[])
-		tlineyz3.set_data([],[])
-		targetyz3.set_data([],[])
+	slinexy4.set_data([],[])
+	flinexy4.set_data([],[])
+	tlinexy4.set_data([],[])
+	targetxy4.set_data([],[])
 
-		slinexz4.set_data([],[])
-		flinexz4.set_data([],[])
-		tlinexz4.set_data([],[])
-		targetxz4.set_data([],[])
-
-		slinexy4.set_data([],[])
-		flinexy4.set_data([],[])
-		tlinexy4.set_data([],[])
-		targetxy4.set_data([],[])
-
-		slineyz4.set_data([],[])
-		flineyz4.set_data([],[])
-		tlineyz4.set_data([],[])
-		targetyz4.set_data([],[])
+	slineyz4.set_data([],[])
+	flineyz4.set_data([],[])
+	tlineyz4.set_data([],[])
+	targetyz4.set_data([],[])
 
 	return slinexz1, flinexz1, tlinexz1, targetxz1, \
 			slinexy1, flinexy1, tlinexy1, targetxy1, \
@@ -320,10 +323,10 @@ def animate(i):
 
 	# solve for angles for each leg
 
-	angs1[i], angf1[i], angt1[i] = getServoAng(x1[i], y1[i], z1[i], ls, lf, lt)
-	angs2[i], angf2[i], angt2[i] = getServoAng(x2[i], y2[i], z2[i], ls, lf, lt)
-	angs3[i], angf3[i], angt3[i] = getServoAng(x3[i], y3[i], z3[i], ls, lf, lt)
-	angs4[i], angf4[i], angt4[i] = getServoAng(x4[i], y4[i], z4[i], ls, lf, lt)
+	angs1[i], angf1[i], angt1[i] = getServoAng(x1[i], y1[i], z1[i], ls, lf, lt, 1)
+	angs2[i], angf2[i], angt2[i] = getServoAng(x2[i], y2[i], z2[i], ls, lf, lt, 2)
+	angs3[i], angf3[i], angt3[i] = getServoAng(x3[i], y3[i], z3[i], ls, lf, lt, 3)
+	angs4[i], angf4[i], angt4[i] = getServoAng(x4[i], y4[i], z4[i], ls, lf, lt, 4)
 
 
 	# ----------------------------
@@ -333,7 +336,7 @@ def animate(i):
 	# given these angles, solve for the limb positions for simulation
 
 	xs1 = [0, 0]
-	ys1 = [0, -ls*cos(angs1[i])]
+	ys1 = [wspine, wspine-ls*cos(angs1[i])]
 	zs1 = [0, -ls*sin(angs1[i])]
 
 	xf1 = [xs1[1], xs1[1] - lf*cos(angf1[i])]
@@ -345,12 +348,12 @@ def animate(i):
 	zt1 = [zf1[1], zf1[1] - lt*sin(angt1[i] + angf1[i])*cos(angs1[i])]
 
 	xtg1 = [x1[i], x1[i]]
-	ytg1 = [y1[i], y1[i]]
+	ytg1 = [wspine+y1[i], wspine+y1[i]]
 	ztg1 = [z1[i], z1[i]]
 
 
 	xs2 = [0, 0]
-	ys2 = [-wspine, -wspine-ls*cos(angs2[i])]
+	ys2 = [0, -ls*cos(angs2[i])]
 	zs2 = [0, -ls*sin(angs2[i])]
 
 	xf2 = [xs2[1], xs2[1] - lf*cos(angf2[i])]
@@ -362,12 +365,12 @@ def animate(i):
 	zt2 = [zf2[1], zf2[1] - lt*sin(angt2[i] + angf2[i])*cos(angs2[i])]
 
 	xtg2 = [x2[i], x2[i]]
-	ytg2 = [-wspine+y2[i], -wspine+y2[i]]
+	ytg2 = [y2[i], y2[i]]
 	ztg2 = [z2[i], z2[i]]
 
 
 	xs3 = [-lspine, -lspine]
-	ys3 = [0, -ls*cos(angs3[i])]
+	ys3 = [wspine, wspine-ls*cos(angs3[i])]
 	zs3 = [0, -ls*sin(angs3[i])]
 
 	xf3 = [xs3[1], xs3[1] - lf*cos(angf3[i])]
@@ -379,12 +382,12 @@ def animate(i):
 	zt3 = [zf3[1], zf3[1] - lt*sin(angt3[i] + angf3[i])*cos(angs3[i])]
 
 	xtg3 = [x3[i]-lspine, x3[i]-lspine]
-	ytg3 = [y3[i], y3[i]]
+	ytg3 = [wspine+y3[i], wspine+y3[i]]
 	ztg3 = [z3[i], z3[i]]
 
 
 	xs4 = [-lspine, -lspine]
-	ys4 = [-wspine, -wspine-ls*cos(angs4[i])]
+	ys4 = [0, -ls*cos(angs4[i])]
 	zs4 = [0, -ls*sin(angs4[i])]
 
 	xf4 = [xs4[1], xs4[1] - lf*cos(angf4[i])]
@@ -396,76 +399,75 @@ def animate(i):
 	zt4 = [zf4[1], zf4[1] - lt*sin(angt4[i] + angf4[i])*cos(angs4[i])]
 
 	xtg4 = [x4[i]-lspine, x4[i]-lspine]
-	ytg4 = [-wspine+y4[i], -wspine+y4[i]]
+	ytg4 = [y4[i], y4[i]]
 	ztg4 = [z4[i], z4[i]]
 
 
 	# write calculated position to limb elements in figure
 
-	if (1==1):
-		slinexz1.set_data(xs1, zs1)
-		flinexz1.set_data(xf1, zf1)
-		tlinexz1.set_data(xt1, zt1)
-		targetxz1.set_data(xtg1, ztg1)
+	slinexz1.set_data(xs1, zs1)
+	flinexz1.set_data(xf1, zf1)
+	tlinexz1.set_data(xt1, zt1)
+	targetxz1.set_data(xtg1, ztg1)
 
-		slinexy1.set_data(xs1, ys1)
-		flinexy1.set_data(xf1, yf1)
-		tlinexy1.set_data(xt1, yt1)
-		targetxy1.set_data(xtg1, ytg1)
+	slinexy1.set_data(xs1, ys1)
+	flinexy1.set_data(xf1, yf1)
+	tlinexy1.set_data(xt1, yt1)
+	targetxy1.set_data(xtg1, ytg1)
 
-		slineyz1.set_data(ys1, zs1)
-		flineyz1.set_data(yf1, zf1)
-		tlineyz1.set_data(yt1, zt1)
-		targetyz1.set_data(ytg1, ztg1)
+	slineyz1.set_data(ys1, zs1)
+	flineyz1.set_data(yf1, zf1)
+	tlineyz1.set_data(yt1, zt1)
+	targetyz1.set_data(ytg1, ztg1)
 
-		slinexz2.set_data(xs2, zs2)
-		flinexz2.set_data(xf2, zf2)
-		tlinexz2.set_data(xt2, zt2)
-		targetxz2.set_data(xtg2, ztg2)
+	slinexz2.set_data(xs2, zs2)
+	flinexz2.set_data(xf2, zf2)
+	tlinexz2.set_data(xt2, zt2)
+	targetxz2.set_data(xtg2, ztg2)
 
-		slinexy2.set_data(xs2, ys2)
-		flinexy2.set_data(xf2, yf2)
-		tlinexy2.set_data(xt2, yt2)
-		targetxy2.set_data(xtg2, ytg2)
+	slinexy2.set_data(xs2, ys2)
+	flinexy2.set_data(xf2, yf2)
+	tlinexy2.set_data(xt2, yt2)
+	targetxy2.set_data(xtg2, ytg2)
 
-		slineyz2.set_data(ys2, zs2)
-		flineyz2.set_data(yf2, zf2)
-		tlineyz2.set_data(yt2, zt2)
-		targetyz2.set_data(ytg2, ztg2)
+	slineyz2.set_data(ys2, zs2)
+	flineyz2.set_data(yf2, zf2)
+	tlineyz2.set_data(yt2, zt2)
+	targetyz2.set_data(ytg2, ztg2)
 
-		slinexz3.set_data(xs3, zs3)
-		flinexz3.set_data(xf3, zf3)
-		tlinexz3.set_data(xt3, zt3)
-		targetxz3.set_data(xtg3, ztg3)
+	slinexz3.set_data(xs3, zs3)
+	flinexz3.set_data(xf3, zf3)
+	tlinexz3.set_data(xt3, zt3)
+	targetxz3.set_data(xtg3, ztg3)
 
-		slinexy3.set_data(xs3, ys3)
-		flinexy3.set_data(xf3, yf3)
-		tlinexy3.set_data(xt3, yt3)
-		targetxy3.set_data(xtg3, ytg3)
+	slinexy3.set_data(xs3, ys3)
+	flinexy3.set_data(xf3, yf3)
+	tlinexy3.set_data(xt3, yt3)
+	targetxy3.set_data(xtg3, ytg3)
 
-		slineyz3.set_data(ys3, zs3)
-		flineyz3.set_data(yf3, zf3)
-		tlineyz3.set_data(yt3, zt3)
-		targetyz3.set_data(ytg3, ztg3)
+	slineyz3.set_data(ys3, zs3)
+	flineyz3.set_data(yf3, zf3)
+	tlineyz3.set_data(yt3, zt3)
+	targetyz3.set_data(ytg3, ztg3)
 
-		slinexz4.set_data(xs4, zs4)
-		flinexz4.set_data(xf4, zf4)
-		tlinexz4.set_data(xt4, zt4)
-		targetxz4.set_data(xtg4, ztg4)
+	slinexz4.set_data(xs4, zs4)
+	flinexz4.set_data(xf4, zf4)
+	tlinexz4.set_data(xt4, zt4)
+	targetxz4.set_data(xtg4, ztg4)
 
-		slinexy4.set_data(xs4, ys4)
-		flinexy4.set_data(xf4, yf4)
-		tlinexy4.set_data(xt4, yt4)
-		targetxy4.set_data(xtg4, ytg4)
+	slinexy4.set_data(xs4, ys4)
+	flinexy4.set_data(xf4, yf4)
+	tlinexy4.set_data(xt4, yt4)
+	targetxy4.set_data(xtg4, ytg4)
 
-		slineyz4.set_data(ys4, zs4)
-		flineyz4.set_data(yf4, zf4)
-		tlineyz4.set_data(yt4, zt4)
-		targetyz4.set_data(ytg4, ztg4)
+	slineyz4.set_data(ys4, zs4)
+	flineyz4.set_data(yf4, zf4)
+	tlineyz4.set_data(yt4, zt4)
+	targetyz4.set_data(ytg4, ztg4)
 
-		spinexz.set_data([-lspine,0],[0,0])
-		spinexy.set_data([-lspine,0],[wspine/2,wspine/2])
-		spineyz.set_data([0,wspine],[0,0])
+	spinexz.set_data([-lspine,0],[0,0])
+	spinexy.set_data([-lspine,0],[wspine/2,wspine/2])
+	spineyz.set_data([0,wspine],[0,0])
 
 
 	# create annotations with live updates about foot position and joint angles (for leg 1)
