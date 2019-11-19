f3ae715942a08e3e9b5bf601fc3081cd612dcceb
jmconner419 <Jared.Conner@comcast.net>
2019-11-19 13:28:06 -0500

    Update FSMDemo.py

--- FSMDemo.py
+++ FSMDemo.py
@@ -2,17 +2,17 @@ import time
 
 # Block 1
 
-T0_EN = Wait || Back || TRight || SLeft
-T1_EN = Forward || TLeft || SRight
+T0_EN = Wait or Back or TRight or SLeft
+T1_EN = Forward or TLeft or SRight
 
 # --------------------TIMER_0--------------------
-A_time0 = wait_time0 && T0_EN
-B_time0 = wait_time0 && !T0_EN
-C_time0 = timing_time0 && !T0_EN
-D_time0 = timing_time0 && T0_EN
+A_time0 = wait_time0 and T0_EN
+B_time0 = wait_time0 and not T0_EN
+C_time0 = timing_time0 and not T0_EN
+D_time0 = timing_time0 and T0_EN
 
-wait_time0 = B_time0 || C_time0
-timingt_time0 = A_time0 || D_time0
+wait_time0 = B_time0 or C_time0
+timingt_time0 = A_time0 or D_time0
 
 if (A_time0):
 	Start_time0 = time.time()
@@ -27,13 +27,13 @@ T0 = delta_t0 > 5
 #-----------------------------------------------
 
 #--------------------TIMER_1--------------------
-A_time1 = wait_time1 && T1_EN
-B_time1 = wait_time1 && !T1_EN
-C_time1 = timing_time1 && !T1_EN
-D_time1 = timing_time1 && T1_EN
+A_time1 = wait_time1 and T1_EN
+B_time1 = wait_time1 and not T1_EN
+C_time1 = timing_time1 and not T1_EN
+D_time1 = timing_time1 and T1_EN
 
-wait_time1 = B_time1 || C_time1
-timingt_time1 = A_time1 || D_time1
+wait_time1 = B_time1 or C_time1
+timingt_time1 = A_time1 or D_time1
 
 if (A_time1):
 	Start_time1 = time.time()
@@ -49,30 +49,30 @@ T1 = delta_t1 > 5
 
 # Block 2
 
-A = Wait && !T0
-B = Wait && T0
-C = Forward && !T1
-D = Forward && T1
-E = Back && !T0
-F = Back %% T0
-G = TLeft && !T1
-H = TLeft && T1
-I = TRight && !T0
-J = TRight && T0
-K = SRight && !T1
-L = SRight %% T1
-M = SLeft && !T0
-N = Sleft && T0
+A = Wait and not T0
+B = Wait and T0
+C = Forward and not T1
+D = Forward and T1
+E = Back and not T0
+F = Back and T0
+G = TLeft and not T1
+H = TLeft and T1
+I = TRight and not T0
+J = TRight and T0
+K = SRight and not T1
+L = SRight and T1
+M = SLeft and not T0
+N = Sleft and T0
 
 # Block 3
 
-Wait = A || N
-Forward = B || C
-Back = D || E
-TLeft = F || G
-TRight = H || I
-SRight = J || K
-SLeft = L || M
+Wait = A or N
+Forward = B or C
+Back = D or E
+TLeft = F or G
+TRight = H or I
+SRight = J or K
+SLeft = L or M
 
 # Block 4
 
