1f0ff68a5a4a0f88907e7e3c28740265264bb061
Ryan Fainor <fainorr@lafayette.edu>
2020-03-12 08:03:31 -0400

    Update lidar_compare.py

--- LIDAR/compare_scans/lidar_compare.py
+++ LIDAR/compare_scans/lidar_compare.py
@@ -10,7 +10,7 @@ from matplotlib import _color_data
 
 # --- SCAN FILE AND PARAMETERS ---
 
-scan_data = 'LIDAR/compare_scans/fifth_people.txt'
+scan_data = 'LIDAR/compare_scans/aec_lobby_corner.txt'
 
 obst_size = 5           # number of consecutive dots
 safe_range = 1.5         # search ranges for obstacles
